7095d5815ed01a7e897cc5bacc81246aa1569676
Victor Bebnev <victor.bebnev@xperience.ai>
2021-07-30 13:17:11 +0300

    Added Background removal task and vis

--- modelplace_api/visualization.py
+++ modelplace_api/visualization.py
@@ -713,3 +713,21 @@ def draw_countable_tracks(
 
 def create_gif(images: List[np.ndarray], save_path: str, fps: int = 1) -> None:
     imageio.mimsave(save_path, images, format="GIF-FI", fps=fps, quantizer="nq")
+
+
+def create_image(image: np.ndarray, save_path: str) -> None:
+    cv2.imwrite(save_path, image)
+
+
+def draw_background_removal(image: np.ndarray, detection: Mask) -> np.ndarray:
+    foreground_mask_id = detection.classes.index("foreground")
+    if len(detection.mask["binary"]) > foreground_mask_id:
+        decoded_mask = decode_coco_rle(detection.mask["binary"][foreground_mask_id])
+        idx = decoded_mask == 0
+        image[idx] = (255, 255, 255)
+        alfa_channel = decoded_mask.astype(image.dtype)[:, :, np.newaxis] * 255
+    else:
+        alfa_channel = np.ones((image.shape[0], image.shape[1], 1), dtype=image.dtype)
+
+    image = np.concatenate((image, alfa_channel), axis=2)
+    return image
